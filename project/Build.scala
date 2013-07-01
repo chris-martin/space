cef6a25559c46588f2e60b2f3b63ac7672eb92d8
Christopher Martin <ch.martin@gmail.com>
2013-07-01 02:54:19 -0400

    add scalac option -language:implicitConversions

--- project/Build.scala
+++ project/Build.scala
@@ -11,6 +11,7 @@ object Build extends Build {
       base = file(base),
       settings = Project.defaultSettings ++ Seq(
         scalaVersion := "2.10.2",
+        scalacOptions := Seq("-language:implicitConversions"),
         libraryDependencies ++= Seq(
           "org.scalatest" % "scalatest_2.10" % "1.9.1" % "test"
         )
