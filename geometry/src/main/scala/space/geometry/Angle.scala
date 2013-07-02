7f5636e2dca04ee38dff111f74c80ec1df49fcca
Christopher Martin <ch.martin@gmail.com>
2013-07-02 02:41:44 -0400

    add Angle[S] trait, Radians[S], Degrees[S]

---
+++ geometry/src/main/scala/space/geometry/Angle.scala
@@ -0,0 +1,34 @@
+package space.geometry
+
+import math.Pi
+
+trait Angle[S] extends ImplicitScalar[S] {
+
+  def toRadians: Radians[S]
+  def toDegrees: Degrees[S]
+}
+
+object Angle {
+
+  val radiansPerDegree = Pi / 180
+  val degreesPerRadian = 180 / Pi
+}
+
+case class Radians[S](x: S)(implicit val scalar: Scalar[S]) extends Angle[S] {
+
+  import scalar._
+
+  override def toRadians: Radians[S] = this
+  override def toDegrees: Degrees[S] = Degrees(x * Angle.degreesPerRadian)
+}
+
+/**
+ * @param x radian value in the range [ -180, 180 ]
+ */
+case class Degrees[S](x: S)(implicit val scalar: Scalar[S]) extends Angle[S] {
+
+  import scalar._
+
+  override def toRadians: Radians[S] = Radians(x * Angle.radiansPerDegree)
+  override def toDegrees: Degrees[S] = this
+}
