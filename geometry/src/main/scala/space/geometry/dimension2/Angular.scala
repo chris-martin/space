e7217eb1b70944b22428df118dc14a15159a8bc3
Christopher Martin <ch.martin@gmail.com>
2013-07-04 03:51:00 -0400

    add arithmetic to Angular types

--- geometry/src/main/scala/space/geometry/dimension2/Angular.scala
+++ geometry/src/main/scala/space/geometry/dimension2/Angular.scala
@@ -1,40 +1,61 @@
 package space.geometry.dimension2
 
-sealed trait Angular extends Any {
+import math.Pi
+
+sealed trait Angular[A <: Angular[A]] extends Any {
 
   def toDouble: Double
 
   def sine: Double = math.sin(toDouble)
-
   def cosine: Double = math.cos(toDouble)
+
+  protected def companion: AngularCompanion[A]
+
+  def +(that: A): A = companion(toDouble + that.toDouble)
+  def -(that: A): A = companion(toDouble - that.toDouble)
+
+  def *(s: Double): A = companion(toDouble * s)
+  def /(s: Double): A = companion(toDouble / s)
 
 }
 
-class Radians (val toDouble: Double) extends AnyVal with Angular
+sealed trait AngularCompanion[T] {
 
-object Radians {
+  def apply(a: Double): T
 
-  def apply(a: Double): Radians = new Radians(a)
+  def halfCircle: T = apply(Pi)
 
+}
+
+class Radians (val toDouble: Double) extends AnyVal with Angular[Radians] {
+  override protected def companion: AngularCompanion[Radians] = Radians
+}
+
+object Radians extends AngularCompanion[Radians] {
+  override def apply(a: Double): Radians = new Radians(a)
 }
 
 /**
  * @param toDouble in the range (-Pi, Pi]
  */
-class Angle private (val toDouble: Double) extends AnyVal with Angular
+class Angle private (val toDouble: Double) extends AnyVal with Angular[Angle] {
+  override protected def companion: AngularCompanion[Angle] = Angle
+}
 
-object Angle {
+object Angle extends AngularCompanion[Angle] {
 
   import math.Pi
 
   val twoPi = 2 * Pi
 
-  def apply(a: Double): Angle = new Angle(
+  override def apply(a: Double): Angle = new Angle(
     a % twoPi match {
       case b if b >   Pi => b - twoPi
       case b if b <= -Pi => b + twoPi
       case b             => b
     }
   )
+
+  def apply(x: Double, y: Double): Angle = new Angle(math.atan2(y, x))
 
 }
