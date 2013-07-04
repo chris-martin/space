8a4577effd87fa2f6946eb93ad1b952a6d3e1334
Christopher Martin <ch.martin@gmail.com>
2013-07-03 23:32:36 -0400

    add remainder method to Scalar

--- geometry/src/main/scala/space/geometry/Scalar.scala
+++ geometry/src/main/scala/space/geometry/Scalar.scala
@@ -7,13 +7,19 @@ trait Scalar[S] {
   implicit def fromNumeric[N](x: N)(implicit numeric: Numeric[N]): S
 
   def squareRoot(a: S): S = fromNumeric(math.sqrt(a.toDouble()))
+
   def square(a: S) = a * a
+
+  /** The remainder of dividing a/b.
+    */
+  def remainder(a: S, b: S): S
 
   /** Some additional operations that aren't provided by `Fractional`.
     */
   implicit class Ops(a: S) {
     def square: S = Scalar.this.square(a)
     def squareRoot: S = Scalar.this.squareRoot(a)
+    def remainder(b: S) = Scalar.this.remainder(a, b)
   }
 
   // Also include the implicits provided by `Fractional`.
@@ -35,6 +41,8 @@ object Scalar {
 
     override protected val fractional = implicitly[Fractional[Float]]
 
+    override def remainder(a: Float, b: Float): Float = a % b
+
   }
 
   implicit object ImplicitFloatIsScalar extends FloatIsScalar
@@ -46,6 +54,8 @@ object Scalar {
 
     override protected val fractional = implicitly[Fractional[Double]]
 
+    override def remainder(a: Double, b: Double): Double = a % b
+
   }
 
   implicit object ImplicitDoubleIsScalar extends DoubleIsScalar
@@ -56,6 +66,8 @@ object Scalar {
       BigDecimal(numeric.mkNumericOps(x).toDouble())
 
     override protected val fractional = implicitly[Fractional[BigDecimal]]
+
+    override def remainder(a: BigDecimal, b: BigDecimal) = a % b
 
   }
 
