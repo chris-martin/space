1d2a5c5c1aecffc66043b7cdf82722ec7470dd3c
Christopher Martin <ch.martin@gmail.com>
2013-07-08 04:43:15 -0400

    remove K-combinator, no longer in use

--- geometry/src/main/scala/space/geometry/package.scala
+++ geometry/src/main/scala/space/geometry/package.scala
@@ -2,15 +2,6 @@ package space
 
 package object geometry {
 
-  implicit class RichAny[A](val x: A) extends AnyVal {
-
-    /** The K (or "Kestrel") combinator.
-      * Also known as "tap" in Ruby.
-      */
-    def K(f: A => Unit): A = { f(x); x }
-
-  }
-
   implicit class RichDouble(val x: Double) extends AnyVal {
 
     def square: Double = x * x
