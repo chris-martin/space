797861eb2edba1f1fe765f2a0b04bcb66c6bd7c1
Christopher Martin <ch.martin@gmail.com>
2013-07-13 01:54:11 -0400

    move Angle-related classes into Angle.scala

--- geometry/src/main/scala/space/geometry/dimension2/Angle.scala
+++ geometry/src/main/scala/space/geometry/dimension2/Angle.scala
@@ -19,3 +19,82 @@ trait Angle[A <: Angle[A]] extends Any {
 }
 
 object Angle extends ArbitraryRadiansCompanion
+
+trait AngleCompanion[A] {
+
+  def apply(a: Double): A
+
+  def halfCircle: A = apply(Pi)
+  def circle: A = apply(twoPi)
+
+}
+
+class ArbitraryRadians (val toDouble: Double)
+  extends AnyVal with Angle[ArbitraryRadians] {
+
+  override protected def companion: AngleCompanion[ArbitraryRadians] = ArbitraryRadians
+
+  override def toString = "ArbitraryRadians(%f)" format toDouble
+
+}
+
+trait ArbitraryRadiansCompanion extends AngleCompanion[ArbitraryRadians] {
+
+  override def apply(a: Double): ArbitraryRadians = new ArbitraryRadians(a)
+
+}
+
+object ArbitraryRadians extends ArbitraryRadiansCompanion
+
+/** @param toDouble in the range [-Pi, Pi)
+  */
+class CircleRadians private (val toDouble: Double)
+  extends AnyVal with Angle[CircleRadians] {
+
+  override protected def companion: AngleCompanion[CircleRadians] = CircleRadians
+
+  override def toString = "CircleRadians(%f)" format toDouble
+
+}
+
+object CircleRadians extends AngleCompanion[CircleRadians] {
+
+  override def apply(a: Double): CircleRadians =
+    new CircleRadians(
+      a % twoPi match {
+        case b  if b >= Pi  =>  b - twoPi
+        case b  if b < -Pi  =>  b + twoPi
+        case b              =>  b
+      }
+    )
+
+  def apply(x: Double, y: Double): CircleRadians =
+    new CircleRadians(math.atan2(y, x))
+
+}
+
+/** @param toDouble in the range [0, Pi)
+  */
+class SemicircleRadians private (val toDouble: Double)
+  extends AnyVal with Angle[SemicircleRadians] {
+
+  override protected def companion: AngleCompanion[SemicircleRadians] = SemicircleRadians
+
+  override def toString = "SemicircleRadians(%f)" format toDouble
+
+}
+
+object SemicircleRadians extends AngleCompanion[SemicircleRadians] {
+
+  override def apply(a: Double): SemicircleRadians =
+    new SemicircleRadians(
+      a % Pi match {
+        case b  if b < 0  => b + Pi
+        case b            => b
+      }
+    )
+
+  def apply(x: Double, y: Double): SemicircleRadians =
+    SemicircleRadians(math.atan2(y, x))
+
+}
--- geometry/src/main/scala/space/geometry/dimension2/ArbitraryRadiansCompanion.scala
+++
@@ -1,8 +0,0 @@
-package space.geometry
-package dimension2
-
-trait ArbitraryRadiansCompanion extends AngleCompanion[ArbitraryRadians] {
-
-  override def apply(a: Double): ArbitraryRadians = new ArbitraryRadians(a)
-
-}
