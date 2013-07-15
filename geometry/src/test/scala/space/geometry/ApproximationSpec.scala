a5ccc5274f2da66180968353c4b9b4faeef48016
Christopher Martin <ch.martin@gmail.com>
2013-07-14 21:40:00 -0400

    use FreeSpec for ApproximationSpec

--- geometry/src/test/scala/space/geometry/ApproximationSpec.scala
+++ geometry/src/test/scala/space/geometry/ApproximationSpec.scala
@@ -1,20 +1,25 @@
 package space.geometry
 
-class ApproximationSpec extends org.scalatest.FunSpec {
+class ApproximationSpec extends org.scalatest.FreeSpec {
 
   implicit val t: Tolerance = 0.0001
 
-  it ("It can be used by mixing in the ImplicitApproximationOperator trait") {
+  "It can be used by mixing in the ImplicitApproximationOperator trait" in {
+
     new Object with ImplicitApproximationOperator {
       assert( 5.4 =~ 5.400000001 )
       assert( 5.4 !=~ 5.41 )
     }
+
   }
 
-  it ("It can be used by importing Approximation._") {
+  "It can be used by importing Approximation._" in {
+
     import Approximation._
+
     assert( 5.4 =~ 5.400000001 )
     assert( 5.4 !=~ 5.41 )
+
   }
 
 }
