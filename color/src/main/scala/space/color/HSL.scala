5de6ca07e6af3faa6bbd07d1cd18ad919ef1355b
Christopher Martin <ch.martin@gmail.com>
2014-01-20 03:23:47 -0500

    Bugfix in toRGB methods

--- color/src/main/scala/space/color/HSL.scala
+++ color/src/main/scala/space/color/HSL.scala
@@ -16,12 +16,12 @@ lightness: ColorValue) extends Color with SpecificColorType[HSL] {
     val x: Double = chroma * ( 1 - abs(h % 2 - 1) )
 
     val rgb: (Double, Double, Double) =
-    if      (0 until 1 contains h) (chroma, x, 0)
-    else if (1 until 2 contains h) (x, chroma, 0)
-    else if (2 until 3 contains h) (0, chroma, x)
-    else if (3 until 4 contains h) (0, x, chroma)
-    else if (4 until 5 contains h) (x, 0, chroma)
-    else                           (chroma, 0, x)
+    if      (h < 1) (chroma, x, 0)
+    else if (h < 2) (x, chroma, 0)
+    else if (h < 3) (0, chroma, x)
+    else if (h < 4) (0, x, chroma)
+    else if (h < 5) (x, 0, chroma)
+    else            (chroma, 0, x)
 
     val m: Double = lightness - chroma / 2
 
